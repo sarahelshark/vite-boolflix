b9b43992760f08c2c86892ea80939d07cf958daa
sarahelshark <sarahelsh183@gmail.com>
2024-02-19 19:05:55 +0100

    test ajax call

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <script>
+import axios from "axios";
 export default {
-  name:'App',
+  name: "App",
+  mounted() {
+    axios
+      .get(
+        'https://api.themoviedb.org/3/movie/11?api_key=b8a3cc58e76a00cf47574cfa4f055fb3b8a3cc58e76a00cf47574cfa4f055fb3'
+      )
+      .then((response) => {
+        console.log(response);
+      })
+      .catch(error =>{
+        console.group(error);
+      })
+  },
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
 };
 </script>
-
 <template>
-  <h1>test</h1>
+  <h1 @click="error">hello</h1>
 </template>
-
-<style ></style>
+<style></style>
---
+++ src/state.js
@@ -0,0 +1,8 @@
+import  {reactive} from 'vue'
+
+export const state = reactive ({
+    //state (unique source of truth )
+    movie_api_url: "https://api.themoviedb.org/3/movie/11?api_key=b8a3cc58e76a00cf47574cfa4f055fb3b8a3cc58e76a00cf47574cfa4f055fb3",
+
+   
+})
