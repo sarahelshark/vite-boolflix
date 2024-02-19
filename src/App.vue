13eb394dbeef67109a723f305a130d169876790f
sarahelshark <sarahelsh183@gmail.com>
2024-02-19 19:29:57 +0100

    fix wrong url

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ export default {
   mounted() {
     axios
       .get(
-        'https://api.themoviedb.org/3/movie/11?api_key=b8a3cc58e76a00cf47574cfa4f055fb3b8a3cc58e76a00cf47574cfa4f055fb3'
+        'https://api.themoviedb.org/3/movie/11?api_key=b8a3cc58e76a00cf47574cfa4f055fb3'
       )
       .then((response) => {
         console.log(response);
