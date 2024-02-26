1f15b6f94b7bd588e4b81fa2ef4aee8ac162882f
sarahelshark <sarahelsh183@gmail.com>
2024-02-26 14:39:02 +0100

    cleanup + comments in state.js

--- src/state.js
+++ src/state.js
@@ -1,96 +1,105 @@
-import  {reactive} from 'vue'
+import { reactive } from "vue";
 import axios from "axios";
 
-export const state = reactive ({
+export const state = reactive({
+  /**reactive object- global state  */
 
-/**reactive object- global state  */
+  //#region reactive datas
+  showDiv: false,
+  searchText: "",
+  //#endregion reactive datas
 
-showDiv: false,
-searchText: "",
-api_url_tv:"https://api.themoviedb.org/3/search/tv?api_key=b8a3cc58e76a00cf47574cfa4f055fb3&language=it-IT",
-    api_url:"https://api.themoviedb.org/3/search/movie?language=it-IT&api_key=b8a3cc58e76a00cf47574cfa4f055fb3",
-    img_prefix: `https://image.tmdb.org/t/p/w342/`,
-    movieCards:[],
-    tvCards:[],
-    filteredMovieCards: [],
+  //#region arrays 
+  movieCards: [],
+  tvCards: [],
+  filteredMovieCards: [],
 
-    filterResults(showDiv) {
+  //#endregion arrays 
 
-      this.showDiv = false;
-      //resetto il messaggio in pagina se si effettua un'altra ricerca dopo il messaggio informativo 
-      
-      /*costruisco qui dentro una chiamata ajax dove costruisco il nuovo url> quella dentro fetchdata  */
-      const url = `${state.api_url}&query=${state.searchText}`;
+  //#region URLs 
+  api_url_tv:
+    "https://api.themoviedb.org/3/search/tv?api_key=b8a3cc58e76a00cf47574cfa4f055fb3&language=it-IT",
+  api_url:
+    "https://api.themoviedb.org/3/search/movie?language=it-IT&api_key=b8a3cc58e76a00cf47574cfa4f055fb3",
 
-      
-      axios
-        .get(url)
-        .then((response) => {
-          state.movieCards = response.data.results;
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+  img_prefix: `https://image.tmdb.org/t/p/w342/`,
 
-        const urlTV=`${state.api_url_tv}&query=${state.searchText}`;
-    const urlTV_unfiltered=`${state.api_url_tv}&query=a`
-      axios
-      .get(urlTV)
+  //#endregion URLs 
+
+  //#region methods
+  
+  filterResults(showDiv) {
+    this.showDiv = false;
+    //resetto il messaggio in pagina se si effettua un'altra ricerca dopo il messaggio informativo
+
+    /*costruisco qui dentro una chiamata ajax MOVIES dove costruisco il nuovo url> quella dentro fetchdata  */
+    const url = `${state.api_url}&query=${state.searchText}`;
+
+    axios
+      .get(url)
       .then((response) => {
-         
-          state.tvCards = response.data.results;
-           console.log(response.data.results)      
-        
+        state.movieCards = response.data.results;
       })
       .catch((error) => {
-          console.error(error);
-        });
+        console.error(error);
+      });
 
+    /*costruisco qui dentro una chiamata ajax  SERIE TV dove costruisco il nuovo url> quella dentro fetchdata  */
 
-      this.searchText = ""; // Clear the input
+    const urlTV = `${state.api_url_tv}&query=${state.searchText}`;
 
-      //faccio apparire messaggio in pagina> lo trasformo in una funzione migliore dopo...
-      
-    },
-
-    fetchMovieData(url) {
-        //console.log(state.api_url);
-        const apiKey = "b8a3cc58e76a00cf47574cfa4f055fb3";
-        const apiUrl = "https://api.themoviedb.org/3/search/movie";
-        axios
-          .get(apiUrl, {
-            params: {
-              language: "it-IT",
-              api_key: apiKey,
-              query: "a",
-            },
-          })
-          .then((response) => {
-            console.log(response.data.results);
-            this.movieCards = response.data.results; //carico array cards con i dati della api
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-
-      starsRate(vote) {
-        return Math.round(vote / 2);
-      },
-
-    fetchTVSeriesData(urlTV){
-      axios
+    axios
       .get(urlTV)
       .then((response) => {
-         
-          state.tvCards = response.data.results;
-           console.log(response.data.results)      
-        
+        state.tvCards = response.data.results;
+        console.log(response.data.results);
       })
       .catch((error) => {
-          console.error(error);
-        });
+        console.error(error);
+      });
 
-    } //abbino SECONDA CHIAMATA
-},
-)
+    this.searchText = ""; // Clear the input
+
+    //faccio apparire messaggio in pagina> lo trasformo in una funzione migliore dopo...
+  },
+
+  fetchMovieData(url) {
+    //console.log(state.api_url);
+    const apiKey = "b8a3cc58e76a00cf47574cfa4f055fb3";
+    const apiUrl = "https://api.themoviedb.org/3/search/movie";
+    axios
+      .get(apiUrl, {
+        params: {
+          language: "it-IT",
+          api_key: apiKey,
+          query: "a",
+        },
+      })
+      .then((response) => {
+        console.log(response.data.results);
+        this.movieCards = response.data.results; //carico array cards con i dati della api
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
+  fetchTVSeriesData(urlTV) {
+    axios
+      .get(urlTV)
+      .then((response) => {
+        state.tvCards = response.data.results;
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
+  starsRate(vote) {
+    return Math.round(vote / 2);
+  },
+
+  //#endregion methods
+  
+});
