8ca84c2114ea9ebbd92f725479af2a0c0c73a256
sarahelshark <sarahelsh183@gmail.com>
2024-03-09 18:32:13 +0100

    fix alt + feat add descriptions

--- src/components/FilteredMoviesRender.vue
+++ src/components/FilteredMoviesRender.vue
@@ -13,7 +13,7 @@ export default {
   mounted() {
     /*RENDER MOVIES HOME PAGE- 1^ chiamata ajax*/
     state.fetchMovieData(state.api_url);
-    state.fetchTVSeriesData(state.api_url_tv)
+    state.fetchTVSeriesData(state.api_url_tv);
   },
 };
 </script>
@@ -21,16 +21,13 @@ export default {
 <template>
   <div class="card col-4 col-lg col-md col-sm" v-for="card in state.movieCards">
     <div class="frontofcard">
-      <img :src="state.img_prefix + card.poster_path" alt="" />
+      <img :src="state.img_prefix + card.poster_path" :alt="card.title" />
     </div>
     <div class="backofcard">
       <h3>{{ card.title }}</h3>
       <ul>
-        <li>{{ card.original_title }}</li>
-        <lang-flag
-      :iso="card.original_language === 'cn' ? 'zh' : card.original_language"
-    />
-          
+        <li>Titolo Originale: {{ card.original_title }}</li>
+        <li><lang-flag :iso="card.original_language === 'cn' ? 'zh' : card.original_language"/></li>
         <li>Voto: {{ state.starsRate(card.vote_average) }}</li>
         <i
           v-for="i in state.starsRate(card.vote_average)"
@@ -40,18 +37,21 @@ export default {
           v-for="i in 5 - state.starsRate(card.vote_average)"
           class="fa-regular fa-star"
         ></i>
+        <li v-if="card.overview.length < 200">{{ card.overview }}</li>
+        <li v-else="card.overview.length > 200">
+          <a
+            >Per vedere la descrizione completa di
+            "{{ card.title }}" <strong>click me!</strong> </a
+          >
+        </li>
       </ul>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 /*#region of cards rotating */
 
-.frontofcard {
-  font-size: 14px;
-}
 .card {
   height: 400px;
   width: 300px;
@@ -60,18 +60,18 @@ export default {
   transition: all 0.8s ease-in;
 }
 .frontofcard,
-.backofcard  {
-    height: 90%;
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.5rem;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    color: var(--bool-light);
-    padding: 0.5rem 0.5rem;
+.backofcard {
+  height: 90%;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  position: absolute;
+  color: var(--bool-light);
+  padding: 0.5rem 0.5rem;
 }
 .card:hover {
   transform: rotateY(180deg);
@@ -91,23 +91,22 @@ export default {
   width: calc(100% / 4);
 }
 
-
-@media screen and (max-width: 1024px){
+@media screen and (max-width: 1024px) {
   .col-lg {
-  width: calc(100% / 3);
-}
+    width: calc(100% / 3);
+  }
 }
 
-@media screen and (max-width: 868px){
+@media screen and (max-width: 868px) {
   .col-md {
-  width: calc(100% / 2);
+    width: calc(100% / 2);
+  }
 }
-}
-@media screen and (max-width: 679px){
+@media screen and (max-width: 679px) {
   .col-sm {
-  width: 70%;
-  margin: auto;
-  margin-bottom: 1rem;
-}
+    width: 70%;
+    margin: auto;
+    margin-bottom: 1rem;
+  }
 }
 </style>
--- src/components/FilteredTVSeriesRender.vue
+++ src/components/FilteredTVSeriesRender.vue
@@ -24,16 +24,13 @@ export default {
 <div class="card col-4" v-for="serieTv in state.tvCards">
     
     <div class="frontofcard">
-      <img :src="state.img_prefix + serieTv.poster_path" alt="" />
+      <img :src="state.img_prefix + serieTv.poster_path" :alt="serieTv.name" />
     </div>
     <div class="backofcard">
       <h3>{{ serieTv.name }}</h3>
       <ul>
         <li>{{ serieTv.original_name }}</li>
-        
-        <lang-flag
-      :iso="serieTv.original_language === 'cn' ? 'zh' : serieTv.original_language"
-    />
+         <li><lang-flag :iso="serieTv.original_language === 'cn' ? 'zh' : serieTv.original_language " /></li>
         <li>Voto: {{ state.starsRate(serieTv.vote_average) }}</li>
         <i
           v-for="i in state.starsRate(serieTv.vote_average)"
@@ -43,19 +40,21 @@ export default {
           v-for="i in 5 - state.starsRate(serieTv.vote_average)"
           class="fa-regular fa-star"
         ></i>
+        <li v-if="serieTv.overview.length < 200">{{ serieTv.overview }}</li>
+        <li v-else="serieTv.overview.length > 200">
+          <a
+            >Per vedere la descrizione completa di
+            "{{ serieTv.name }}" <strong>click me!</strong> </a
+          >
+        </li>
       </ul>
     </div>
   </div>
 
 </template>
-
 <style scoped>
-
 /*#region of cards rotating */
 
-.frontofcard {
-  font-size: 14px;
-}
 .card {
   height: 400px;
   width: 300px;
@@ -64,18 +63,18 @@ export default {
   transition: all 0.8s ease-in;
 }
 .frontofcard,
-.backofcard  {
-    height: 90%;
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    gap: 0.5rem;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    position: absolute;
-    color: var(--bool-light);
-    padding: 0.5rem 0.5rem;
+.backofcard {
+  height: 90%;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  position: absolute;
+  color: var(--bool-light);
+  padding: 0.5rem 0.5rem;
 }
 .card:hover {
   transform: rotateY(180deg);
@@ -95,23 +94,22 @@ export default {
   width: calc(100% / 4);
 }
 
-
-@media screen and (max-width: 1024px){
+@media screen and (max-width: 1024px) {
   .col-lg {
-  width: calc(100% / 3);
-}
+    width: calc(100% / 3);
+  }
 }
 
-@media screen and (max-width: 868px){
+@media screen and (max-width: 868px) {
   .col-md {
-  width: calc(100% / 2);
+    width: calc(100% / 2);
+  }
 }
-}
-@media screen and (max-width: 679px){
+@media screen and (max-width: 679px) {
   .col-sm {
-  width: 70%;
-  margin: auto;
-  margin-bottom: 1rem;
-}
+    width: 70%;
+    margin: auto;
+    margin-bottom: 1rem;
+  }
 }
 </style>
