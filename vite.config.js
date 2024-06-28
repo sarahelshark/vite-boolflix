dc36456b8b584ff1ad6c106ad14135823417d100
sarahelshark <sarahelsh183@gmail.com>
2024-06-28 12:18:41 +0200

    [test]

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    outDir: 'docs', // Change output directory to 'docs'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
